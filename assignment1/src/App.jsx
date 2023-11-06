c4973a82317d2a1654de69bc918446237e03e059
SamuelDebruin <145690649+SamuelDebruin@users.noreply.github.com>
2023-11-06 11:43:28 -0500

    again update

--- assignment1/src/App.jsx
+++ assignment1/src/App.jsx
@@ -1,16 +1,18 @@
-import "./App.css"; 
+import "./App.css";
+//imports the grocrie file onto the website  
 import GroceriesApp from "./GroceriesApp";
 
-
+//h2 displays the website title at a certen size
 function App() {
   return(
     <>
+    
       <h2 className="name">Grocreies App</h2>
       <GroceriesApp/>
 
     </>
   
-  );
+  )
 }
 
 
